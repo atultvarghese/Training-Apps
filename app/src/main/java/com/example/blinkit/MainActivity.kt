69dee3014d74839802056de1c676b419dc1b9b9b
atultvarghese <atultvarghese@gmail.com>
2024-03-28 23:59:58 +0530

    remember me in main page and some design changes

--- app/src/main/java/com/example/blinkit/MainActivity.kt
+++ app/src/main/java/com/example/blinkit/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.blinkit
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,32 +33,43 @@ class MainActivity : AppCompatActivity() {
         var passwordText = findViewById<EditText>(R.id.main_password)
         var loginButton = findViewById<Button>(R.id.main_login)
         var registerButton = findViewById<Button>(R.id.main_register)
+        var rememberMe = findViewById<CheckBox>(R.id.main_remember_me_checkbox)
 
         registerButton.setOnClickListener(){
             var registerIntent = Intent(this, RegistrationActivity::class.java)
             startActivity(registerIntent)
         }
 
-//        var db = Room.databaseBuilder(this, UsersDatabase::class.java, "users")
-//            .fallbackToDestructiveMigration().build()
-
         var h = Handler()
 
+        var sp : SharedPreferences = getSharedPreferences("remember_me", MODE_PRIVATE)
         var db : UsersDatabase = UsersDatabase.getDataBase(this)
+
+        usernameText.setText(sp.getString("username", ""))
+        passwordText.setText(sp.getString("password", ""))
 
         loginButton.setOnClickListener() {
             GlobalScope.launch {
                 var user = db.usersDbCreate().checkUserExisting(usernameText.text.toString(), passwordText.text.toString())
-
+                var editor = sp.edit()
                 Log.i("login", "$user")
                 if (user.isEmpty()){
                     Log.i("login", "User name doesn't exists")
+                    h.post { Toast.makeText(this@MainActivity, "Invalid user name or password", Toast.LENGTH_LONG).show() }
+
                 } else {
-                    Log.i("login", "User name exists $user")
-//                    withContext(Dispatchers.Main) {
-//                        var profile = Intent(this@MainActivity, DashBoard::class.java)
-//                    }
-                    h.post { startActivity(Intent(this@MainActivity, DashBoardActivity::class.java)) }
+
+                    if (rememberMe.isChecked){
+                        editor.putString("username", usernameText.text.toString())
+                        editor.putString("password", passwordText.text.toString())
+                    } else {
+                        editor.remove("username")
+                        editor.remove("password")
+                    }
+                    h.post {
+                        Toast.makeText(this@MainActivity, "Welcome ${usernameText.text.toString()} ", Toast.LENGTH_LONG).show()
+                        startActivity(Intent(this@MainActivity, DashBoardActivity::class.java))
+                    }
                 }
             }
         }
