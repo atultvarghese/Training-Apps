f8408bbb25cc2fd0c379c4c2bf01e0f56da720a7
atultvarghese <atultvarghese@gmail.com>
2024-05-21 16:50:05 +0530

    Get sensors example data AAOS

---
+++ automotive/src/main/java/com/atultvarghese/sensorsconnect/SensorViewModel.kt
@@ -0,0 +1,27 @@
+package com.atultvarghese.sensorsconnect
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class SensorViewModel : ViewModel() {
+    private val _lightSensorData = MutableStateFlow(0f)
+    val lightSensorData: StateFlow<Float> get() = _lightSensorData
+
+    private val _accelerometerData = MutableStateFlow(0f)
+    val accelerometerData: StateFlow<Float> get() = _accelerometerData
+
+    fun updateLightSensorData(value: Float) {
+        viewModelScope.launch {
+            _lightSensorData.value = value
+        }
+    }
+
+    fun updateAccelerometerData(value: Float) {
+        viewModelScope.launch {
+            _accelerometerData.value = value
+        }
+    }
+}
